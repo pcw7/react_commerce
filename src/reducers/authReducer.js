fe36daea54d48a02db7078a60a3fab5f96f9e59f
Parkchanwoo <parkchanwoo7@naver.com>
2024-08-21 10:15:20 +0900

    로그인안한상태에서 장바구니 추가하면 로그인페이지로 가는데, 로그인 후 로그아웃하고 장바구니 추가를 누르면 로그아웃된 장바구니에 들어가는 현상 수정

--- src/reducers/authReducer.js
+++ src/reducers/authReducer.js
@@ -1,30 +1,40 @@
 const initialState = {
-    email: '',
-    password: '',
-    isAuthenticated: false,
-    user: null,
-    isSeller: false,
-    error: null,
-    userId: null,
-  };
-  
-  const authReducer = (state = initialState, action) => {
-    switch (action.type) {
-      case 'SET_EMAIL':
-        return { ...state, email: action.payload };
-      case 'SET_PASSWORD':
-        return { ...state, password: action.payload };
-      case 'LOGIN_SUCCESS':
-        return { ...state, isAuthenticated: true, user: action.payload, error: null };
-      case 'LOGIN_FAILURE':
-        return { ...state, isAuthenticated: false, error: action.payload };
-      case 'SET_USER_DATA':
-        return { ...state, isSeller: action.payload.isSeller, userId: action.payload.userId };
-      case 'LOGOUT':
-        return { ...state, isAuthenticated: false, user: null, email: '', password: '', error: null };
-      default:
-        return state;
-    }
-  };
-  
-  export default authReducer;
+  email: '',
+  password: '',
+  isAuthenticated: false,
+  user: null,
+  isSeller: false,
+  error: null,
+  userId: null,
+};
+
+const authReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case 'SET_EMAIL':
+      return { ...state, email: action.payload };
+    case 'SET_PASSWORD':
+      return { ...state, password: action.payload };
+    case 'LOGIN_SUCCESS':
+      return {
+        ...state,
+        isAuthenticated: true,
+        user: action.payload,
+        error: null,
+        userId: action.payload.uid // userId를 로그인 시 저장
+      };
+    case 'LOGIN_FAILURE':
+      return { ...state, isAuthenticated: false, error: action.payload };
+    case 'SET_USER_DATA':
+      return {
+        ...state,
+        isSeller: action.payload.isSeller,
+        userId: action.payload.userId // userId를 설정
+      };
+    case 'LOGOUT':
+      return initialState; // 로그아웃 시 초기 상태로 리셋
+    default:
+      return state;
+  }
+};
+
+export default authReducer;
