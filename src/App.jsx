092690c61796f617cca377edc2da1498ac60f491
Parkchanwoo <parkchanwoo7@naver.com>
2024-08-22 14:57:04 +0900

    번들 줄이기

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,18 @@
-import React from 'react';
+import React, { Suspense, lazy } from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import './App.css';
-
-import SignupForm from './components/views/SignupForm';
-import LoginForm from './components/views/LoginForm';
-import Mypage from './components/views/Mypage';
-import ProductRegistration from './components/views/ProductRegistration';
-import ProductDetail from './components/views/ProductDetail';
-import AllProductsPage from './components/views/AllProductsPage';
 import Navbar from './components/views/Navbar';
 import Cart from './components/views/Cart';
 import { CartProvider } from './context/CarContext.jsx';
-import { AuthProvider } from './context/AuthContext.jsx'; // AuthContext 추가
+import { AuthProvider } from './context/AuthContext.jsx';
+
+const SignupForm = lazy(() => import('./components/views/SignupForm'));
+const LoginForm = lazy(() => import('./components/views/LoginForm'));
+const Mypage = lazy(() => import('./components/views/Mypage'));
+const ProductRegistration = lazy(() => import('./components/views/ProductRegistration'));
+const ProductDetail = lazy(() => import('./components/views/ProductDetail'));
+const AllProductsPage = lazy(() => import('./components/views/AllProductsPage'));
 
 const queryClient = new QueryClient();
 
@@ -24,15 +24,17 @@ function App() {
           <Router>
             <Navbar />
             <Cart />
-            <Routes>
-              <Route path="/" element={<AllProductsPage />} />
-              <Route path="/signup" element={<SignupForm />} />
-              <Route path="/login" element={<LoginForm />} />
-              <Route path="/mypage" element={<Mypage />} />
-              <Route path="/product-registration" element={<ProductRegistration />} />
-              <Route path="/product-registration/:productId" element={<ProductRegistration />} />
-              <Route path="/product/:productId" element={<ProductDetail />} />
-            </Routes>
+            <Suspense fallback={<div>Loading...</div>}>
+              <Routes>
+                <Route path="/" element={<AllProductsPage />} />
+                <Route path="/signup" element={<SignupForm />} />
+                <Route path="/login" element={<LoginForm />} />
+                <Route path="/mypage" element={<Mypage />} />
+                <Route path="/product-registration" element={<ProductRegistration />} />
+                <Route path="/product-registration/:productId" element={<ProductRegistration />} />
+                <Route path="/product/:productId" element={<ProductDetail />} />
+              </Routes>
+            </Suspense>
           </Router>
         </CartProvider>
       </AuthProvider>
@@ -41,3 +43,47 @@ function App() {
 }
 
 export default App;
+
+// import React from 'react';
+// import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+// import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+// import './App.css';
+
+// import SignupForm from './components/views/SignupForm';
+// import LoginForm from './components/views/LoginForm';
+// import Mypage from './components/views/Mypage';
+// import ProductRegistration from './components/views/ProductRegistration';
+// import ProductDetail from './components/views/ProductDetail';
+// import AllProductsPage from './components/views/AllProductsPage';
+// import Navbar from './components/views/Navbar';
+// import Cart from './components/views/Cart';
+// import { CartProvider } from './context/CarContext.jsx';
+// import { AuthProvider } from './context/AuthContext.jsx'; // AuthContext 추가
+
+// const queryClient = new QueryClient();
+
+// function App() {
+//   return (
+//     <QueryClientProvider client={queryClient}>
+//       <AuthProvider>
+//         <CartProvider>
+//           <Router>
+//             <Navbar />
+//             <Cart />
+//             <Routes>
+//               <Route path="/" element={<AllProductsPage />} />
+//               <Route path="/signup" element={<SignupForm />} />
+//               <Route path="/login" element={<LoginForm />} />
+//               <Route path="/mypage" element={<Mypage />} />
+//               <Route path="/product-registration" element={<ProductRegistration />} />
+//               <Route path="/product-registration/:productId" element={<ProductRegistration />} />
+//               <Route path="/product/:productId" element={<ProductDetail />} />
+//             </Routes>
+//           </Router>
+//         </CartProvider>
+//       </AuthProvider>
+//     </QueryClientProvider>
+//   );
+// }
+
+// export default App;
--- vite.config.js
+++ vite.config.js
@@ -2,15 +2,51 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
 import { fileURLToPath } from 'url';
+import { visualizer } from 'rollup-plugin-visualizer';
+
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    visualizer({
+      filename: './dist/stats.html', // 번들 분석 결과를 저장할 파일 경로
+      open: true, // 빌드 후 브라우저에서 분석 결과를 자동으로 열도록 설정
+    }),
+  ],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, 'src/'),
     },
   },
+  build: {
+    rollupOptions: {
+      output: {
+        // 코드 스플리팅 설정
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            return id.toString().split('node_modules/')[1].split('/')[0].toString();
+          }
+        },
+      },
+    },
+  },
 });
+
+// import { defineConfig } from 'vite';
+// import react from '@vitejs/plugin-react';
+// import path from 'path';
+// import { fileURLToPath } from 'url';
+// const __filename = fileURLToPath(import.meta.url);
+// const __dirname = path.dirname(__filename);
+
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+//   resolve: {
+//     alias: {
+//       '@': path.resolve(__dirname, 'src/'),
+//     },
+//   },
+// });
