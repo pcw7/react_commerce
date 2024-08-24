16da611f6a64e6d7142e9da2165d91eb25af8705
Parkchanwoo <parkchanwoo7@naver.com>
2024-08-24 11:43:24 +0900

    파이어베이스 키 재생성 후 .env에서 변수로 사용하도록 수정

--- src/firebase.js
+++ src/firebase.js
@@ -1,25 +1,18 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyCd-eqy7dNU7SZe_n8ykDZK1FcJRCeSIZg",
-    authDomain: "react-commerce-26d4a.firebaseapp.com",
-    projectId: "react-commerce-26d4a",
-    storageBucket: "react-commerce-26d4a.appspot.com",
-    messagingSenderId: "744332631614",
-    appId: "1:744332631614:web:778b7e911d2a8de8107b6f"
+    apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+    authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+    projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+    storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+    appId: import.meta.env.VITE_FIREBASE_APP_ID
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-// React에서 정상적으로 동작하는 지 확인하기 위해서 임시로 export 시켜줍니다. app이 정상적으로 출력되는 것을 확인하고 나면, 지워줍니다.
-// export default app
 
 export const auth = getAuth(app);
 export const db = getFirestore(app);
